8e6757b8a6000a8dd13daa6dfbef5a549889fd43
Teemu Koivisto <tkoivisto456@gmail.com>
2025-01-17 20:13:08 +0200

    initial commit with working boilerplate

---
+++ packages/app/src/lib/Greet.svelte
@@ -0,0 +1,18 @@
+<script>
+  import * as tauriApi from '@tauri-apps/api'
+
+  let name = ''
+  let greetMsg = ''
+
+  async function greet() {
+    console.log('greeting', tauriApi)
+    greetMsg = await tauriApi.core.invoke('greet', { name })
+    console.log('greetMsg', greetMsg)
+  }
+</script>
+
+<div>
+  <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+  <button onclick={greet}>Greet</button>
+  <p>{greetMsg}</p>
+</div>
